3bec7016c4d828eb3aa7f39f9f8b6a770f164b98
mrncstt <mrncstt@gmail.com>
2025-06-20 22:30:51 +0200

    Deploy version funcionando com npm run dev

--- exams-simulator/vite.config.js
+++ exams-simulator/vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/exams-simulator/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['cookie'],
-    },
-  },
 });
